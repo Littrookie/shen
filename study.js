d8be39512d33bb5b229c40d341cda1c3ca799f0c
shen <1074159855@qq.com>
2024-03-09 15:17:17 +0800

    commit from vscode

--- study.js
+++ study.js
@@ -7,8 +7,14 @@ console.log(age);
 
 const a=10;
 //a=111;//会报错，const为常量
+
+//String,Number,Boolean,null,undefined
+
+//console.log(s.toUpperCase())大写
+//console.log(s.toLowerCase())小写
+
 console.log(a);
-//相加转换：去掉"+",用${}
+//连接
 console.log(`My number is ${age} and I am ${a}`);
 //三目运算
 const x=10;
@@ -25,3 +31,33 @@ switch(color){
     default:
         console.log("ether");
 }    
+//for
+for(let i=0;i<10;i++){
+    console.log(i)
+}
+//while
+let i=0;
+while (i<10){
+    console.log(`While Loop Number:${i}`);
+    i++;
+}
+
+//数组
+const number=new Array(1,2,3,4);
+console.log(number);
+
+const fruits=["apple","orange"];
+console.log(fruits);
+//数组末尾加个元素
+fruits.push("mangos");
+//头部
+fruits.unshift("strawberries");
+//删除末尾
+fruits.pop(); 
+//索引
+console.log(fruits.indexOf("orange"));
+
+
+//JSON
+const todoJSON=JSON.stringify(fruits);
+console.log(todoJSON);  
