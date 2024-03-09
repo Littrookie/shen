09e56d867ee9b27a7b9643596dbee2eb14a19443
shen <1074159855@qq.com>
2024-03-09 15:01:46 +0800

    commit from vscode

--- study.js
+++ study.js
@@ -1,5 +1,27 @@
-console.log("aaa");
-//let , const
-//let的值可以被修改，const不能
+console.log("aa");
+// let , const
+// let的值可以被修改，const不能
 let age=30;
-console.log=(age);
+age=40;
+console.log(age);
+
+const a=10;
+//a=111;//会报错，const为常量
+console.log(a);
+//相加转换：去掉"+",用${}
+console.log(`My number is ${age} and I am ${a}`);
+//三目运算
+const x=10;
+const color=x>10?"red":"blue";
+console.log(color); 
+//switch
+switch(color){
+    case "red":
+        console.log("color is red");
+        break;
+    case "blue":
+        console.log("color is blue");
+        break;
+    default:
+        console.log("ether");
+}    
